67dc47685ae218933d610c663ca8c74094585dc1
Nithin Anand <31352669+DrainYou@users.noreply.github.com>
2017-12-06 12:41:54 +0000

    Update front end

--- front_end.py
+++ front_end.py
@@ -1,16 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 from decimal import *
+from serial import *
 
 root = Tk()
 displayed_temp = StringVar()
 displayed_ph = StringVar()
 displayed_stir = StringVar()
+ser = Serial('COM6')
+
+def monitoring():
+    found = ser.readline()
+    print(found)
 
 def update_temppoint():
     if temp_verification(var_values[0].get()) == True:
-        temp_set = Decimal(var_values[0].get())
-        displayed_temp.set("Current temperature setpoint: " + str(temp_set) + "\u2103")
+        temp_set = var_values[0].get()
+        displayed_temp.set("Current temperature setpoint: " + temp_set + "\u2103")
+        ser.write(temp_set.encode('utf-8'))
     else:
         displayed_temp.set("Please enter a value between 25 and 35 (inclusive).")
     ttk.Label(root, textvariable = displayed_temp).grid(row = 3, column = 0) 
@@ -19,8 +26,9 @@ def update_temppoint():
 
 def update_phpoint():
     if ph_verification(var_values[1].get()) == True:
-        ph_set = Decimal(var_values[1].get())
-        displayed_ph.set("Current pH setpoint: " + str(ph_set))
+        ph_set = var_values[1].get()
+        displayed_ph.set("Current pH setpoint: " + ph_set)
+        ser.write(ph_set.encode('utf-8'))
     else:
         displayed_ph.set("Please enter a value between 3 and 7 (inclusive).")
     ttk.Label(root, textvariable = displayed_ph).grid(row = 4, column = 0) 
@@ -29,8 +37,9 @@ def update_phpoint():
 
 def update_stirpoint():
     if stir_verification(var_values[2].get()) == True:
-        stir_set = Decimal(var_values[2].get())
-        displayed_stir.set("Current stirring setpoint: " + str(stir_set) + " RPM")
+        stir_set = var_values[2].get()
+        displayed_stir.set("Current stirring setpoint: " + stir_set + " RPM")
+        ser.write(stir_set.encode('utf-8'))
     else:
         displayed_stir.set("Please enter a value between 500 and 1500 (inclusive).")
     ttk.Label(root, textvariable = displayed_stir).grid(row = 5, column = 0) 
@@ -78,5 +87,5 @@ for i in range (0, 3):
     ttk.Label(root, text="Update " + controllables[i] + " Setpoint " + units[i]).grid(row=i, column=0)
 
 
-
+monitoring()
 root.mainloop()
